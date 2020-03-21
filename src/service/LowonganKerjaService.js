c2e749f6a18ae076959adbbaf85bfcb6e61e01e0
Muhammad Arief Darmawan <darmawanarief71@gmail.com>
2020-03-21 22:37:33 +0700

    feat:deleteLoker

--- src/service/LowonganKerjaService.js
+++ src/service/LowonganKerjaService.js
@@ -6,6 +6,11 @@ class LowonganKerjaService {
     retrieveAllLoker() {
         return axios.get('http://localhost:8081/api/listLoker');
     }
+
+    deleteLoker(idLowongan){
+        var link = "http://localhost:8081/api/hapusLoker/" + idLowongan
+        return axios.delete(link);
+    }
 }
 
 export default new LowonganKerjaService();
