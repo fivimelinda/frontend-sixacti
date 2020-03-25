4daa604015f0fa6adfd9bc0348adf99aa374ff1b
Muhammad Arief Darmawan <darmawanarief71@gmail.com>
2020-03-25 13:55:27 +0700

    fix:layout tampilan list loker

--- src/components/LowonganKerja/ListLokerComponent.vue
+++ src/components/LowonganKerja/ListLokerComponent.vue
@@ -4,11 +4,45 @@
     <div v-if="message" class="alert alert-success">
       {{message}}
     </div>
-    <div class="container">
+    <div class="container-fluid">
       <div class="row mt-5 mb-3 ml-1">
         <button class="btn btn-success w-25" v-on:click="addLokerClicked()">Add</button>
       </div>
-      <table class="table">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4" v-for="loker in loker" v-bind:key="loker.idLowongan">
+          <div class="card mb-4" id="cardLoker">
+            <div class="card-header" id="cardHeader">
+              <span id="titleCard">{{loker.idLowongan}}</span>
+            </div>
+            <div class="card-body">
+              <span class="mb-1" id="lokerDesc">{{loker.deskripsi}}</span>
+              <p class="mt-1 mb-3">Departement</p>
+              <p id="periodeDesc">Periode</p>
+
+            </div>
+            <div class="card-footer" id="card-footer">
+              <div class="row">
+                <div class="col-6 mb-3">
+                  <button class="btn btn-danger w-100" v-on:click="detailLokerClicked(loker.idLowongan)">Detail</button>
+                </div>
+                <div class="col-6 mb-3">
+                  <button class="btn btn-light border-danger w-100" id="hapusBtn"  v-on:click="deleteLokerClicked(loker.idLowongan)">Hapus</button>
+                </div>
+              </div>
+              <!-- <div style="height:5px;background-color:#C53751; "></div> -->
+              
+            </div>
+          </div>
+        </div>
+        
+        
+      </div>
+      
+
+
+
+
+      <!-- <table class="table">
         <thead>
           <tr>
             <th>Id</th>
@@ -31,7 +65,7 @@
             </td>
           </tr>
         </tbody>
-      </table>
+      </table> -->
     </div>
   </div>
 </template>
@@ -60,12 +94,11 @@ export default {
             this.refreshLoker();
           });
         },
-        detailLokerClicked(idLowongan){
-          var link = "detailLoker/" + idLowongan;
-          this.$router.push(link);
-        },
         addLokerClicked(){
           this.$router.push("/buatLoker");
+        },
+        detailLokerClicked(idLowongan){
+          this.$router.push("/detailLoker/"+idLowongan);
         }
 
 
@@ -78,4 +111,56 @@ export default {
 </script>
 
 <style>
+
+#cardLoker{
+  /* height: 300px; */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: left;
+  min-height: 312px
+}
+
+#cardHeader{
+  border-style: none;
+  background-color: white;
+}
+
+#card-footer{
+  border-top-style: none ;
+  border-bottom-style: solid;
+  border-bottom-width: 10px;
+  border-bottom-color: #C53751;
+  background-color: white;
+
+}
+
+#titleCard{
+  color: #C53751;
+  font-weight: bold;
+  font-size: 32px;
+
+}
+
+#lokerDesc{
+  font-family: Archivo;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 15px;
+
+  color: #736B6B;
+}
+
+#periodeDesc{
+  font-family: Archivo;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 15px;
+
+  color: #7D0022;
+}
+
+#hapusBtn{
+  color: #C53751;
+}
 </style>
