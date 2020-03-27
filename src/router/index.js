3b27e9acaeaaf2dca4bfa898687690e0a6a1f41d
Nur Rifandy <nur.rifandy@ui.ac.id>
2020-03-27 21:31:22 +0700

    routing

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,75 @@ import Home from '../views/Home.vue'
 import RequestLowongan from '../components/RequestLowongan/RequestLowongan.vue'
 import formCreateLoker from '../components/LowonganKerja/formCreateLoker.vue'
 import listLoker from '../components/LowonganKerja/ListLokerComponent.vue'
+import { LayoutPlugin } from 'bootstrap-vue'
 
 Vue.use(VueRouter)
 
+const emptyComponent = {
+  template : '<router-view><router-view>'
+}
+
 const routes = [
+  // home path
+  {
+    path:'*',
+    redirect:{name:'Home'}
+  },
+  
+  // path untuk authentikasi 
+  {
+    path: '/auth',
+    component: emptyComponent,
+    children :[
+      {
+        name:'login',
+        path:'login',
+        component:emptyComponent
+      },
+      {
+        name:'signup',
+        path:'signup',
+        component : emptyComponent
+      },
+      {
+        name: 'change-password',
+        path: 'change-password',
+        component: emptyComponent
+      },
+      {
+        path: '',
+        redirect: {
+          name:'login'
+        }
+      }
+    ]
+  },
+
+  // path for error
+  {
+    path : '/404',
+    component: emptyComponent,
+    children :[
+      {
+        name:'not-found',
+        path:'not-found',
+        component:emptyComponent
+      }
+    ]
+  },
+  {
+    path:'/502',
+    component:emptyComponent,
+    children:[
+      {
+        name:'internal-server',
+        path:'internal-server',
+        component:emptyComponent
+      }
+    ]
+  },
+
+  // main path
   {
     path: '/',
     name: 'Home',
@@ -38,8 +103,4 @@ const routes = [
   }
 ]
 
-const router = new VueRouter({
-  routes
-})
-
-export default router
+export default new VueRouter({routes})
