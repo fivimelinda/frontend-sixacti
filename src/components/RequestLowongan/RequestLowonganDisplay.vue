79f52e9a49508bf2edc623cc72c22f1808e73304
Grahana Daffa Herlambang <grahanadaffa1@gmail.com>
2020-03-30 09:52:37 +0700

    table in requestlowongan

--- src/components/RequestLowongan/RequestLowonganDisplay.vue
+++ src/components/RequestLowongan/RequestLowonganDisplay.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <h1>Request Lowongan display all</h1>
-    <div>
+    <!-- <div>
         <ul v-for ="req in requestLowongan" :key="req.id">
             <li>
                 <span>
@@ -9,7 +9,27 @@
                 </span>
             </li>
         </ul>
-    </div>
+    </div> -->
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No.</th>
+                <th scope="col">Judul</th>
+                <th scope="col">Jumlah</th>
+                <th scope="col">Tanggal diiginkan</th>
+                <th scope="col">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="req in requestLowongan" :key="req.id">
+                <th scope=row>{{req.id}}</th>
+                <td>{{req.jobTitle}}</td>
+                <td>{{req.jumlah}}</td>
+                <td>{{req.dateWanted}}</td>
+                <td>{{req.status}}</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 </template>
 
