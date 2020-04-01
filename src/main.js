ffb22c2ac8e585a52170474a2e7fc877b5acc54d
Nur Rifandy <nur.rifandy@ui.ac.id>
2020-04-01 14:36:38 +0700

    jangan di merge

--- src/main.js
+++ src/main.js
@@ -5,12 +5,37 @@ import vuetify from './plugins/vuetify'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { ColorThemePlugin } from './service/vuestic-ui'
+import store from './store/index'
+//import router from './router/index'
+import { VuesticPlugin } from './service/vuestic-ui/components'
+
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
+
+
+//staging
+Vue.use(VuesticPlugin)
+//Vue.use(YmapPlugin)
+//Vue.use(VueClipboard)
+
+Vue.use(ColorThemePlugin, {
+  // override colors here.
+})
+
+router.beforeEach((to, from, next) => {
+  store.commit('setLoading', true)
+  next()
+})
+
+router.afterEach((to, from) => {
+  store.commit('setLoading', false)
+})
+
 
 new Vue({
   router,
