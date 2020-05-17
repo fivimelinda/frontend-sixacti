ff1768d4ae67efb69c54773a721d155945ebe564
oktafia9 <oktafia99@gmail.com>
2020-05-17 14:48:49 +0700

    auth

--- src/service/AtributService.js
+++ src/service/AtributService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-
+import authHeader from './AuthHeader'
 const URI = 'http://localhost:8081/api';
 
 
 class AtributService {
 
     updateStatus(id_atribut, atribut){
-        return axios.put(`${URI}/ubahStatusAtribut/${id_atribut}`, atribut);
+        return axios.put(`${URI}/ubahStatusAtribut/${id_atribut}`, atribut, { headers:authHeader() });
     }
 
     //tambah atribut
