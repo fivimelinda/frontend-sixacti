57a296147a694ee0ebd66d013ead968abf1ddabe
Muhammad Arief Darmawan <darmawanarief71@gmail.com>
2020-05-17 14:20:12 +0700

    add autorisasi untuk fungsi UC1 dan UC2 3

--- src/service/LamaranService.js
+++ src/service/LamaranService.js
@@ -1,17 +1,18 @@
 import axios from "axios";
+import authHeader from './AuthHeader';
 
 const URI = 'http://localhost:8081/api';
 
 class LamaranService {
 
     addLamaran(idLowongan, lamaran){
-        return axios.post(URI + "/addLamaran/" + idLowongan, lamaran);
+        return axios.post(URI + "/addLamaran/" + idLowongan, lamaran, { headers:authHeader() });
     }
     getLamaranById(id){
-        return axios.get(URI + "/detailLamaran/" + id) ;
+        return axios.get(URI + "/detailLamaran/" + id, { headers:authHeader() }) ;
     }
     setStatus(id, lamaran){
-        return axios.put(URI + "/setStatus/" + id, lamaran);
+        return axios.put(URI + "/setStatus/" + id, lamaran, { headers:authHeader() });
     }
 
 }
