a41551da11d4bacf3ef8eaded66b091a15899234
Nur Rifandy <nur.rifandy@ui.ac.id>
2020-05-18 09:02:15 +0700

    styling signup admin

--- src/components/Auth/SignUp/SignUpAdmin.vue
+++ src/components/Auth/SignUp/SignUpAdmin.vue
@@ -1,17 +1,19 @@
 <template>
+    <div class="background">
     <v-container>
+        <div class="jumbotron" style="background-color:transparent">
         <div class="jumbotron" style="background-color:transparent">
             <v-card
             max-width="350px"
             class="mx-auto my-auto"
-            style="box-shadow:none">
+            style="">
                 <b-card>
                     <b-card-text class="text-center">
-                        <div class="h5 m-0">
+                        <div class="h5 m-0 primary-color">
                             Daftar Sekarang
                         </div>
-                        <div class="mb-5" style="font-size:12px">
-                            Sudah punya akun PT. Xacti? <strong><a href="/auth/login">Masuk</a></strong>
+                        <div class="mb-5 secondary-color" style="font-size:12px">
+                            Sudah punya akun PT. Xacti? <strong><a class="primary-color" href="/auth/login">Masuk</a></strong>
                         </div>
                         <b-form
                         class="m-0">
@@ -21,9 +23,10 @@
                                         <b-icon-person-fill></b-icon-person-fill>
                                     </b-input-group-prepend>
                                     <b-form-input
+                                    class="input"
                                     size="sm"
                                     id="username"
-                                    v-model="user.username"
+                                    v-model="username"
                                     type="text"
                                     required
                                     placeholder="Username">
@@ -33,12 +36,33 @@
                             <b-form-group>
                                 <b-input-group size="sm">
                                     <b-input-group-prepend is-text>
+                                        <b-icon-briefcase-fill></b-icon-briefcase-fill>
+                                    </b-input-group-prepend>
+                                    <template>
+
+                                    </template>
+                                    <b-form-select size="sm"
+                                        class="input"
+                                        v-model='value'
+                                        :options="options"
+                                    >
+                                        <template v-slot:first>
+                                        <!-- This is required to prevent bugs with Safari -->
+                                        <option disabled value=null>Choose a role...</option>
+                                        </template>
+                                    </b-form-select>
+                                </b-input-group>
+                            </b-form-group>
+                            <b-form-group>
+                                <b-input-group size="sm">
+                                    <b-input-group-prepend is-text>
                                         <b-icon-lock-fill></b-icon-lock-fill>
                                     </b-input-group-prepend>
                                     <b-form-input
+                                    class="input"
                                     size="sm"
                                     id="password"
-                                    v-model="user.password"
+                                    v-model="password"
                                     type="password"
                                     required
                                     placeholder="password">
@@ -46,8 +70,22 @@
                                 </b-input-group>
                             </b-form-group>
                             <b-form-group>
-                                <!-- prop `add-on-change` is needed to enable adding tags vie the `change` event -->
-
+                                <b-input-group size="sm">
+                                    <b-input-group-prepend is-text>
+                                        <b-icon-lock-fill></b-icon-lock-fill>
+                                    </b-input-group-prepend>
+                                    <b-form-input
+                                    class="input"
+                                    size="sm"
+                                    id="password"
+                                    v-model="confirm_password"
+                                    type="password"
+                                    required
+                                    placeholder="konfirmasi password">
+                                </b-form-input>
+                                </b-input-group>
+                            </b-form-group>
+                            <!-- <b-form-group>
                                 <b-form-tags style="" v-model="value" size="sm" add-on-change no-outer-focus class="mb-2">
                                     <template v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }">
                                     <b-input-group size="sm">
@@ -63,7 +101,7 @@
                                             :options="availableOptions"
                                         >
                                             <template v-slot:first>
-                                            <!-- This is required to prevent bugs with Safari -->
+                                           
                                             <option disabled value="">Choose a role...</option>
                                             </template>
                                         </b-form-select>
@@ -82,81 +120,141 @@
                                     </ul>
                                     </template>
                                 </b-form-tags>
-                                </b-form-group>
-                                <b-button size="sm" class="btn btn-block" @click="handleRegister()">Daftar</b-button>
+                            </b-form-group> -->
+
+                            
+                                <b-button size="sm" class="btn btn-block button-primary" @click="handleRegister()">Daftar</b-button>
+                                <div class="mt-1">
+                                    <p class="typo__p" v-if="submitStatus === 'OK'" style="color:rgb(60, 233, 54)">Sign Up Success!</p>
+                                    <p class="typo__p" v-if="submitStatus === 'ERROR'" style="color:red">Please fill the form correctly.</p>
+                                    <p class="typo__p" v-if="submitStatus === 'PENDING'" style="color:rgb(246, 226, 47)">Please Wait! Signup...</p>
+                                    <p class="typo__p" v-if="submitStatus === 'FAILED'" style="color:rgb(246, 226, 47)">{{message}}</p>
+                                </div>
                         </b-form>
+                        <div class="mt-5 pr-5 pl-5" style="font-size:12px" >
+                            Dengan mendaftar, saya menetujui <a href="">Syarat dan Ketentuan</a>
+                            serta <a href="">Kebijakan Privasi.</a>
+                        </div>
                     </b-card-text>
                 </b-card>
             </v-card>
-            <div>{{value}}</div>
         </div>
-    </v-container>    
+        </div>
+    </v-container>
+    </div>
 </template>
 <script>
-import User from '../../../models/User';
+// import User from '../../../models/User';
 import {BIconLockFill, BIconPersonFill, BIconBriefcaseFill} from 'bootstrap-vue'
+import {required, minLength, sameAs} from 'vuelidate/lib/validators'
 
 export default {
     name: 'Register',
-  components:{
-      BIconBriefcaseFill,
-      BIconLockFill,
-      BIconPersonFill
-  },
-  data() {
-    return {
-      object:{
-          username:'',
-          password:'',
-          role:[]
-      },
-      user: new User('', ''),
-      confirm_password:"",
-      submitted: false,
-      successful: false,
-      message: '',
-      value: [],
-      options: ['karyawan tetap', 'karyawan kontrak', 'admin', 'manager', 'assistant', 'section', 'departement'],
-    };
-  },
-  computed: {
-    loggedIn() {
-      return this.$store.state.auth.status.loggedIn;
+    components:{
+        BIconBriefcaseFill,
+        BIconLockFill,
+        BIconPersonFill
     },
-    availableOptions() {
-        return this.options.filter(opt => this.value.indexOf(opt) === -1)
-      }
-  },
-  mounted() {
-    if (this.loggedIn) {
-      this.$router.push('/auth/signup-admin');
+    validations:{
+        username:{
+            required,
+            minLength:minLength(6)
+        },
+        password:{
+            required,
+            minLength:minLength(6)
+        },
+        confirm_password:{
+            required,
+            sameAsPassword:sameAs('password')
+        }
+    },
+    data() {
+        return {
+        object:{
+            username:'',
+            password:'',
+            role:[]
+        },
+        username:"",
+        password:"",
+        confirm_password:"",
+        submitted: false,
+        successful: false,
+        message: '',
+        value: null,
+        options: ['karyawan tetap', 'karyawan kontrak', 'admin', 'manager', 'assistant', 'section', 'departement'],
+        };
+    },
+    computed: {
+        loggedIn() {
+        return this.$store.state.auth.status.loggedIn;
+        },
+        availableOptions() {
+            return this.options.filter(opt => this.value.indexOf(opt) === -1)
+        }
+    },
+    mounted() {
+        if (this.loggedIn) {
+        this.$router.push('/auth/signup-admin');
+        }
+    },
+    methods: {
+        handleRegister() {
+        this.message = '';
+        this.submitted = true;
+        this.object.username = this.user.username;
+        this.object.password = this.user.password;
+        this.object.role.push(this.value);
+        console.log(this.object);
+        this.$store.dispatch('auth/register', this.object).then(
+                data => {
+                this.message = data.message;
+                this.successful = true;
+                },
+                error => {
+                this.message =
+                    (error.response && error.response.data) ||
+                    error.message ||
+                    error.toString();
+                this.successful = false;
+                }
+            );
+        }
     }
-  },
-  methods: {
-    handleRegister() {
-      this.message = '';
-      this.submitted = true;
-      this.object.username = this.user.username;
-      this.object.password = this.user.password;
-      this.object.role = this.value;
-      console.log(this.object.role);
-      this.$store.dispatch('auth/register', this.object).then(
-            data => {
-              this.message = data.message;
-              this.successful = true;
-            },
-            error => {
-              this.message =
-                (error.response && error.response.data) ||
-                error.message ||
-                error.toString();
-              this.successful = false;
-            }
-          );
     }
-  }
-}
 </script>
 <style lang="scss" scoped>
-
+.background{
+    background-color: #F6EDF0 !important;
+}
+.primary-color{
+    color:#C53751 !important;
+}
+.secondary-color{
+    color:rgb(175, 129, 129);
+}
+.button-primary{
+    color: white !important;
+    background-color: #C53751 !important;
+    border:none !important;
+}
+.typo__p{
+    font-size:12px;
+    text-align: left;
+}
+.input:focus{
+    border:1px solid #C53751;
+    background-color: rgba(197, 55, 81, 0.02) !important;
+}
+.input{
+    font-size: 14px;
+    background-color: rgba(248, 248, 248, 0.726) !important;
+}
+.error-custom{
+    font-size: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: red;
+    text-align: left;
+}
 </style>
