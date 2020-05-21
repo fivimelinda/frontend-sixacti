a6796e2b67c5a9ff3dab2136427dfa4e5ad983cd
fivimelinda <54924175+fivimelinda@users.noreply.github.com>
2020-05-21 18:35:48 +0700

    set state and fixing error feature cuti (#94)
    * fixing error
    
    * try deploy
    
    * setting sidebar
    
    * error handling
    
    * change api url
    
    * edit navbar
    
    * edit signup
    
    * set state and fixing error feature cuti

--- src/components/Profil/Profil.vue
+++ src/components/Profil/Profil.vue
@@ -998,6 +998,10 @@ export default{
                                 "idSect":this.idSect
                             },{headers:authHeader()}).then(res =>{
                                 console.log(res);
+                                const nowUser = JSON.parse(localStorage.getItem('user'));
+                                nowUser.user = this.retStatus.user;
+                                localStorage.user = JSON.stringify(nowUser);
+                                console.log(localStorage.user)
                                 
                             });
                             this.openModal2()
@@ -1026,6 +1030,10 @@ export default{
 
         createPelamar(){
             this.pelamar = axios.get('https://sixacti-api.herokuapp.com/profil/addPelamar/'+this.nik, { headers:authHeader() });
+            const nowUser = JSON.parse(localStorage.getItem('user'));
+            nowUser.user = this.retStatus.user;
+            localStorage.user = JSON.stringify(nowUser);
+            console.log(localStorage.user)
             this.openModal2()
             
             },
